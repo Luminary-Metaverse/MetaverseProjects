16198bf5ac05c05aa018f5f7536d621775767a99
Muhammad Zuhair Zuhdi <joe@frostfire.id>
2023-11-27 17:11:24 +0700

    Test Discord Web Hook

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
-const express = require('express');
+'use strict';
+
 const http = require('http');
-const socketIO = require('socket.io');
+const socket = require('socket.io');
+const server = http.createServer();
+const port = 11100;
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
-const PORT = 1032;
-
-const otherPlayers = {};
-const otherPlayerHands = {};
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.emit('setPlayerId', socket.id);
-  socket.broadcast.emit('playerConnected');
-
-  // Menerima event playerMovement dari klien
-  socket.on('playerMovement', (data) => {
-    socket.broadcast.emit('updatePlayerMovement', data);
-  });
-
-  // Menerima event gravityMovement dari klien
-  socket.on('gravityMovement', (data) => {
-    socket.broadcast.emit('updateGravity', data);
-  });
-
-  // Menerima event updateLeftHandMovement dari klien
-  socket.on('updateLeftHandMovement', (data) => {
-    socket.broadcast.emit('updateLeftHandMovement', data);
-  });
-
-  // Menerima event updateRightHandMovement dari klien
-  socket.on('updateRightHandMovement', (data) => {
-    socket.broadcast.emit('updateRightHandMovement', data);
-  });
-
-  // Menanggapi event disconnect dari klien
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-
-    const disconnectedPlayerId = socket.id;
-
-    // Menghapus pemain dari pemain lain
-    if (otherPlayers[disconnectedPlayerId]) {
-      delete otherPlayers[disconnectedPlayerId];
-      socket.broadcast.emit('updateOtherPlayerDisconnected', disconnectedPlayerId);
-    }
-
-    // Menghapus tangan pemain (kiri) dari objek tangan pemain lain
-    if (otherPlayerHands[disconnectedPlayerId + '_left']) {
-      delete otherPlayerHands[disconnectedPlayerId + '_left'];
-    }
-
-    // Menghapus tangan pemain (kanan) dari objek tangan pemain lain
-    if (otherPlayerHands[disconnectedPlayerId + '_right']) {
-      delete otherPlayerHands[disconnectedPlayerId + '_right'];
-    }
-  });
+var io = socket(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+io.use((socket, next) => {
+    if (socket.handshake.query.token === "UNITY") {
+        next();
+    } else {
+        next(new Error("Authentication error"));
+    }
 });
