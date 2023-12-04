4b5f83017bce9998c9ee435975b630f6cd25d4ce
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2023-12-04 16:04:13 +0700

    feat : add webQL

--- Assets/Samples/Ready Player Me Core/4.0.1/QuickStart/Scripts/ThirdPersonMovement.cs
+++ Assets/Samples/Ready Player Me Core/4.0.1/QuickStart/Scripts/ThirdPersonMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace ReadyPlayerMe.Samples
 {
@@ -30,6 +31,8 @@ namespace ReadyPlayerMe.Samples
         private bool isRunning;
 
         private GroundCheck groundCheck;
+
+        public Animator anim;
         
         private void Awake()
         {
@@ -89,6 +92,7 @@ namespace ReadyPlayerMe.Samples
         public void SetIsRunning(bool running)
         {
             isRunning = running;
+            anim.SetFloat("MoveSpeed", 1);
         }
         
         public bool TryJump()
